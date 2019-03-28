1174cddc79c5747514fca0cba5d999a782462ada
liangtong <liangtongdev@163.com>
2019-03-28 15:32:02 +0800

    Component init

---
+++ LTxConfigDemo/LTxConfigDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LTxConfigDemo
+//
+//  Created by liangtong on 2019/3/28.
+//  Copyright © 2019 LIANGTONG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
