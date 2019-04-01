5f11db596fb93693704a3df78f8826894a8ee519
liangtong <liangtongdev@163.com>
2019-04-01 22:31:19 +0800

    代码结构调整

--- LTxConfig/LTxConfig.h
+++ LTxConfig/LTxConfig.h
@@ -26,52 +26,51 @@ NS_ASSUME_NONNULL_BEGIN
  **/
 - (void)appSetup;
 
-#pragma mark - 颜色
-@property (nonatomic, strong) UIColor* skinColor;
-@property (nonatomic, strong) UIColor* hintColor;
-@property (nonatomic, strong) UIColor* activityViewBackgroundColor;
-@property (nonatomic, strong) UIColor* viewBackgroundColor;
-@property (nonatomic, strong) UIColor* cellContentViewColor;
-@property (nonatomic, assign) CGColorRef cellContentViewShadowColor;
+#pragma mark - 应用配置
+@property (nonatomic, strong) UIColor* skinColor;//皮肤颜色
+@property (nonatomic, strong) UIColor* hintColor;//提示框背景颜色
+@property (nonatomic, strong) UIColor* viewBackgroundColor;//页面背景颜色
+@property (nonatomic, strong) UIColor* cellContentViewColor;//cell背景颜色
 
-#pragma mark
-
-@property (nonatomic, readonly) BOOL isDebug;
-
-#pragma mark - 签名验证
+@property (nonatomic, copy) NSString* appId;//应用编码（ID）
+@property (nonatomic, copy) NSString* pushId;//推送编码
+@property (nonatomic, assign) NSInteger pageSize;//每页(次)条目数量
 @property (nonatomic, copy) NSString* signature;//签名验证时的Token
+@property (nonatomic, assign) BOOL customCameraAlbum;//自定义相册
 
-#pragma mark - host
+#pragma mark - 类别及各种host
+@property (nonatomic, readonly) BOOL isDebug;//是否测试版本
+
 @property (nonatomic, copy) NSString* host;
 @property (nonatomic, copy) NSString* messageHost;
 @property (nonatomic, copy) NSString* eepmHost;
 @property (nonatomic, copy) NSString* serviceHost;
 @property (nonatomic, copy) NSString* shareHost;
+@property (nonatomic, copy) NSString* instalUrl;
 
-#pragma mark - 系统配置
-@property (nonatomic, copy) NSString* appId;
-@property (nonatomic, copy) NSString* pushId;
+#pragma mark - 用户
 @property (nonatomic, copy) NSNumber* userId;
 @property (nonatomic, copy) NSString* userNumber;
-@property (nonatomic, assign) NSInteger pageSize;
 
-#pragma mark - 其他
-@property (nonatomic, copy) NSString* instalUrl;
-@property (nonatomic, copy) NSString* instalTip;
-@property (nonatomic, copy) NSString* aboutTip;
-@property (nonatomic, copy) NSString* loginTip;
-@property (nonatomic, assign) BOOL cameraAlbumCustom;// 保存相片/视频时，是否使用自定义相册
+
 
 #pragma mark - 下载
-//是否后台下载
-@property (nonatomic, readonly) BOOL enableBackgroundDownload;
-//某个时间点上最大的下载个数(大于0)，默认为2
-@property (nonatomic, readonly) NSInteger maxDownloadingCount;
+@property (nonatomic, readonly) BOOL enableBackgroundDownload;//是否后台下载
+@property (nonatomic, readonly) NSInteger maxDownloadingCount;//某个时间点上最大的下载个数(大于0)，默认为2
 
+
+#pragma mark - 提示信息
+@property (nonatomic, copy) NSString* instalTip;//安装提示
+@property (nonatomic, copy) NSString* aboutTip;//关于提示
+@property (nonatomic, copy) NSString* loginTip;//登录页面的提示
 
 #pragma mark - 刷新
-@property (nonatomic, strong) NSArray* refreshHeaderImages;
-@property (nonatomic, strong) NSArray* refreshFooterImages;
+@property (nonatomic, strong) NSArray* refreshHeaderImages;//下拉刷新时的图片
+@property (nonatomic, strong) NSArray* refreshFooterImages;//加载更多时的图片
+
+#pragma mark - 空画面
+@property (nonatomic, strong) UIImage* emptyImage;//空画面图片
+@property (nonatomic, strong) UIImage* errorEmptyImage;//发生错误时的空画面图片
 
 @end
 
--- LTxConfig/LTxConfig.m
+++ LTxConfig/LTxConfig.m
@@ -41,67 +41,82 @@ static LTxConfig *_instance;
     /*颜色*/
     _skinColor = [UIColor colorWithRed:59/255.0 green:145/255.0 blue:233/255.0 alpha:1];
     _hintColor = [UIColor colorWithRed:240/255.0 green:173/255.0 blue:78/255.0 alpha:1];
-    _activityViewBackgroundColor = [UIColor lightGrayColor];
     _viewBackgroundColor = [UIColor colorWithRed:240.0/255.0 green:240.0/255.0 blue:240.0/255.0 alpha:1];
     _cellContentViewColor = [UIColor colorWithRed:230.0/255.0 green:230.0/255.0 blue:230.0/255.0 alpha:1];
-    _cellContentViewShadowColor = [UIColor lightGrayColor].CGColor;
-    
+  
     /*配置参数*/
     NSURL* configFileURL = [[NSBundle mainBundle] URLForResource:@"LTxConfig" withExtension:@"plist"];
     if (configFileURL) {
         NSDictionary* configDic = [NSDictionary dictionaryWithContentsOfURL:configFileURL];
         NSString* type = [configDic objectForKey:@"type"];
         
-        
-        //版本信息
+        //类别及各种host
         _isDebug = [type isEqualToString:@"debug"];
+        NSDictionary* typeDic = [configDic objectForKey:type];
+        _host = [typeDic objectForKey:@"host"];
+        _instalUrl = [typeDic objectForKey:@"instalUrl"];
         
-        //签名验证
-        _signature = [configDic objectForKey:@"signature"];
+        //应用配置
         _appId = [configDic objectForKey:@"appId"];
         _pushId = [configDic objectForKey:@"pushId"];
         _pageSize = [[configDic objectForKey:@"pageSize"] integerValue];
+        _customCameraAlbum = [[configDic objectForKey:@"customCameraAlbum"] boolValue];
+        _signature = [configDic objectForKey:@"signature"];
         
-        _instalTip = [configDic objectForKey:@"instalTip"];
-        _aboutTip = [configDic objectForKey:@"aboutTip"];
-        _loginTip = [configDic objectForKey:@"loginTip"];
-        _cameraAlbumCustom = [[configDic objectForKey:@"cameraAlbumCustom"] boolValue];
         
         //下载
         NSDictionary* downloadConfig = [configDic objectForKey:@"download"];
         _enableBackgroundDownload = [[downloadConfig objectForKey:@"backgroundDownload"] boolValue];
         _maxDownloadingCount = [[downloadConfig objectForKey:@"maxDownloadingCount"] integerValue];
         
-        //HOST
-        NSDictionary* typeDic = [configDic objectForKey:type];
-        _instalUrl = [typeDic objectForKey:@"instalUrl"];
-        _host = [typeDic objectForKey:@"host"];
+        
+        //提示信息
+        NSDictionary* tipsDic = [configDic objectForKey:@"tips"];
+        _instalTip = [tipsDic objectForKey:@"instalTip"];
+        _aboutTip = [tipsDic objectForKey:@"aboutTip"];
+        _loginTip = [tipsDic objectForKey:@"loginTip"];
+        
         
         //刷新
         NSDictionary* refreshDic = [configDic objectForKey:@"refresh"];
         _refreshHeaderImages = [refreshDic objectForKey:@"header"];
         _refreshFooterImages = [refreshDic objectForKey:@"footer"];
         
-        //缓存中读取 - 防止由于host获取失败导致无法继续更新的问题
+        //空画面
+        NSDictionary* emptyDic = [configDic objectForKey:@"empty"];
+        _emptyImage = LTxImageWithName([emptyDic objectForKey:@"empty_image"]);
+        _errorEmptyImage =  LTxImageWithName([emptyDic objectForKey:@"error_image"]);
+        
     }else{//默认配置
-        /*HOST*/
-        _messageHost = @"http://125.46.29.147:8852/eepj_push";
-        _eepmHost = @"http://125.46.29.147:8851/eepm";
-        
-        /*系统配置*/
-        _appId = @"4f424ed4-b0f1-4af7-9567-aef6cd23d01a";
-        _pageSize = 20;
-        
-        /*其他*/
+        /*类别及各种host*/
+        _host = @"http://192.168.1.75:8801/eepj-base-login";
         _instalUrl = @"http://192.168.1.75:8801/eepm";
+        
+        /*应用配置*/
+        _appId = @"";
+        _pushId = @"";
+        _pageSize = 20;
+        _customCameraAlbum = YES;
+        _signature = nil;
+        
+        /*下载*/
+        _enableBackgroundDownload = NO;
+        _maxDownloadingCount = 2;
+        
+        /*提示信息*/
         _instalTip = @"在苹果设备上安装的重要提示：\
         \n1.扫码后,确保“切换到苹果自带的Safari浏览器打开网页”，方能成功安装！\
         \n2.针对iOS9及以上版本的用户，打开本应用时你可能会收到“未受信任的企业级开发者”的提示。此时，你需按照以下步骤手工完成设置（苹果官方最新安全要求）：进入[设置]>[通用]>[描述文件]>[企业级应用]>[Sippr Enginnering Group Co., LTD.]，点击“信任...”或进入[设置]>[通用]>[设备管理]>[Sippr Enginnering Group Co., LTD.]，点击“信任...”。";
         _aboutTip = @"本应用包含极光推送，版权所有(c) 2012, 深圳市和讯华谷信息技术有限公司。";
-        _cameraAlbumCustom = YES;
+        _loginTip = @"";
         
-        _enableBackgroundDownload = NO;
-        _maxDownloadingCount = 1;
+        /*刷新*/
+        _refreshHeaderImages = @[];
+        _refreshFooterImages = @[];
+        
+        /*空画面*/
+        _emptyImage = LTxImageWithName(@"ic_view_empty");
+        _errorEmptyImage = LTxImageWithName(@"ic_view_error_empty");
     }
     
     
